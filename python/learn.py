f5d309e291238bd24aac0627b1138898930d624d
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-05 11:04:38 +0200

    better script

--- python/learn.py
+++ python/learn.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-
 import pandas as pd
 
-from sklearn.tree import DecisionTreeClassifier 
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+import random
+
+from sklearn.tree import DecisionTreeClassifier
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.svm import SVC
+import numpy.random
 
 from sklearn.model_selection import train_test_split
 
-ctor = DecisionTreeClassifier
-# ctor = SVC
-# ctor = LogisticRegression
-# ctor = KNeighborsClassifier
+random.seed(0)
+numpy.random.seed(0)
 
-tbl = pd.read_csv("python/train.csv")
+ctor = DecisionTreeClassifier
+
+tbl = pd.read_csv("data.csv")
 tbl.fillna(0, inplace=True)
 
-X = tbl.drop(
-    ["Survived"],
-    axis=1,
-)
-
+X = tbl.drop(["Survived"], axis=1,)
 Y = tbl[["Survived"]]
 
 X = pd.get_dummies(X)
 Y = pd.get_dummies(Y)
 
+# split the data to train and test
 X_train, X_test, y_train, y_test = train_test_split(X, Y)
 
 alg = ctor()
