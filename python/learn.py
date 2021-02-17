3700f07de46d6368b539d68e66a003335720c407
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-17 03:56:37 +0200

    made the example more beautiful

--- python/learn.py
+++ python/learn.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-import pandas as pd
+import pandas
+import sklearn.tree
+import sklearn.model_selection
 
-import random
-
-from sklearn.tree import DecisionTreeClassifier
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.svm import SVC
-import numpy.random
-
-from sklearn.model_selection import train_test_split
-
-
-tbl = pd.read_csv("data.csv")
+tbl = pandas.read_csv("data.csv")
 tbl.fillna(0, inplace=True)
 
 X = tbl.drop(["Survived"], axis=1,)
 Y = tbl[["Survived"]]
 
-X = pd.get_dummies(X)
-Y = pd.get_dummies(Y)
+# turn all columns numeric, if a column is not numeric replace it with 0/1 columns
+# that represent it's various states. Could produce a lot of columns...
+X = pandas.get_dummies(X)
 
 # split the data to train and test
-X_train, X_test, y_train, y_test = train_test_split(X, Y)
+X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y)
 
-alg = DecisionTreeClassifier(max_depth=18)
-alg.fit(X_train, y_train)
+alg = sklearn.tree.DecisionTreeClassifier()
+alg.fit(X_train, Y_train)
 
-score = alg.score(X_test, y_test)
-score_train = alg.score(X_train, y_train)
-print(f"test score {score}")
-print(f"train score {score_train}")
+score_test = alg.score(X_test, Y_test)
+score_train = alg.score(X_train, Y_train)
+print(f"test score {score_test}, train score {score_train}")
