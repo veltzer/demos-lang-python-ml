0b857318171c801708a3341ace739fbb97d8b4f1
Mark Veltzer <mark.veltzer@gmail.com>
2021-04-13 18:28:54 +0300

    added classic KNN classifier

--- python/learn_k_nearest_neighbours.py
+++ python/learn_k_nearest_neighbours.py
@@ -3,6 +3,10 @@
 import pandas
 import sklearn.model_selection
 import sklearn.neighbors
+import numpy.random
+
+# always get the same results...
+numpy.random.seed(5)
 
 tbl = pandas.read_csv("data.csv")
 tbl.fillna(0, inplace=True)
@@ -17,7 +21,8 @@ X = pandas.get_dummies(X)
 # split the data to train and test
 X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y)
 
-alg = sklearn.neighbors.NearestCentroid()
+# alg = sklearn.neighbors.NearestCentroid()
+alg = sklearn.neighbors.KNeighborsClassifier(n_neighbors=5)
 alg.fit(X_train, Y_train.values.ravel())
 
 score_test = alg.score(X_test, Y_test)
