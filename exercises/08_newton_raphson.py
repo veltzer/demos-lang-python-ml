6a0de200f52dec472c0ca5cebeb08ab873bd45fa
Mark Veltzer <mark.veltzer@gmail.com>
2021-05-12 17:14:27 +0300

    solution to the 08 exercise

---
+++ exercises/08_newton_raphson.py
@@ -0,0 +1,27 @@
+def square(x):
+    return x*x
+
+def third(x):
+    return x*x*x
+
+def identity(x):
+    return x
+
+def newton_raphson(f, initial_guess=1.0, max_iterations=1000, dx=0.001, precision=0.001):
+    x=initial_guess
+    i=0
+    while i<max_iterations:
+        y=f(x)
+        derivitive=(f(x+dx)-f(x-dx))/(2*dx)
+        if abs(derivitive) < precision:
+            return x
+        x+=-y/derivitive
+        # print(f"x is {x}")
+        i+=1
+    return None
+
+result=newton_raphson(identity)
+if result is None:
+    print(f"couldnt find solution")
+else:
+    print(f"found solution {result}")
