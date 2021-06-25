0e50b781ff47b39585f07788b0ee57d6f43f403e
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-25 08:22:20 +0300

    some stuff from kalture course

---
+++ series_examples/function_by_numbers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def my_function(x):
+    if x==1:
+        return 1
+    if x==2:
+        return 4
+    if x==3:
+        return 1979
+
+
+for i in range(1,4):
+    print(my_function(i))
--- series_examples/make_series_from_any_set_of_numbers.py
+++ series_examples/make_series_from_any_set_of_numbers.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def fun(x):
-    sum=0
-    sum+=(x-2)*(x-3)/2+(x-1)*(x-3)*(-4)+(x-1)*(x-2)*(1997/2)
-    return sum
+    return (x-2)*(x-3)/2+(x-1)*(x-3)*(-4)+(x-1)*(x-2)*(1979/2)
 
 
 for i in range(1,4):
