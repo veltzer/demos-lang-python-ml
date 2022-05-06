1ea0f7804993fb395a7c2a00f74a04637bb4324e
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 05:28:03 +0300

    trying to build

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "demos-ml-python"
+project_long_description = "Demos machine learning in the python language"
+project_year_started = 2019
+project_description = "Demos machine learning in the python language"
