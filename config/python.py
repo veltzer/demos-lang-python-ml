2403f2c93cccf6be567df529167c9b95b931437d
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 14:25:36 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,18 +2,19 @@ import config.project
 
 package_name = config.project.project_name
 
+dev_requires = [
+    "pymakehelper",
+    "pydmt",
+    "pyclassifiers",
+]
 install_requires = [
     "sklearn",
     "pandas",
     "numpy",
     "matplotlib",
 ]
-dev_requires = [
-    "pymakehelper",
-    "pydmt",
-    "pyclassifiers",
-]
 
-python_requires = ">=3.9"
-test_os = ["ubuntu-20.04"]
-test_python = ["3.9"]
+python_requires = ">=3.10"
+
+test_os = ["ubuntu-22.04"]
+test_python = ["3.10"]
