0106aa8d476b64afbfa93389345f9e3f48d6fff8
Mark Veltzer <mark.veltzer@gmail.com>
2026-01-30 16:35:28 +0200

    no commit message given

---
+++ src/titanic/clean_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to clean data in machine learning.
+The data is the Titanic data.
+"""
+
+import pandas
+
+df = pandas.read_csv("../data/titanic.csv")
